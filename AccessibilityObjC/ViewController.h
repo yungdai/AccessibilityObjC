c285a3281407276c4f98ec5c0fef34b3a2fc4ecc
Yung Dai <yungchidai@gmail.com>
2015-11-17 21:36:30 -0500

    Initial Commit

---
+++ AccessibilityObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AccessibilityObjC
+//
+//  Created by Yung Dai on 2015-11-17.
+//  Copyright © 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AccessibilityObjCTests/AccessibilityObjCTests.m
@@ -0,0 +1,39 @@
+//
+//  AccessibilityObjCTests.m
+//  AccessibilityObjCTests
+//
+//  Created by Yung Dai on 2015-11-17.
+//  Copyright © 2015 Yung Dai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AccessibilityObjCTests : XCTestCase
+
+@end
+
+@implementation AccessibilityObjCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
